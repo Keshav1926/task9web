4f3638124093e94e4a8283e5f4cbfc30332705b8
Keshav1926 <62308187+Keshav1926@users.noreply.github.com>
2022-05-15 16:58:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.mkdirSync('keshav');
+
+// fs.writeFileSync("keshav/bio.txt", "my name is keshav");
+
+// fs.appendFileSync("keshav/bio.txt", "this is task 9");
+
+// const data = fs.readFileSync("keshav/bio.txt", "utf-8");
+// console.log(data);
+
+// fs.renameSync("keshav/bio.txt", "keshav/myBio.txt");
+
+// fs.unlinkSync("keshav/myBio.txt");
+
+// fs.rmdirSync("keshav");
